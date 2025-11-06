bc93c0501a519aa849bf84f334522410f7b0b3c1
rupalisarcar <rupalisarcar@gmail.com>
2025-11-07 02:31:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+let currentSound;
+buttons.forEach((btn)=>{
+	btn.addEventListener('click',()=>{
+		if(currentSound){
+			currentSound.pause()
+		}
+		const soundFile = btn.getAttribute('data-sound');
+		currentSound = new Audio(soundFile);
+		currentSound.play();
+	})
+})
+
+stopButton.addEventListener('click',()=>{
+	if(currentSound){
+			currentSound.pause();
+		currentSound.currentTime=0;//Reset to the begining;
+		}
+})
